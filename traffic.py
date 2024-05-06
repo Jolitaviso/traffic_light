5062f47ea5a5a606edb4b3242d6622e7eecd4d36
Jolitaviso <jolitavinter@gmail.com>
2024-05-07 00:30:34 +0300

    dictionary

--- traffic.py
+++ traffic.py
@@ -1,44 +1,28 @@
 def count_color_occurrences(file_path):
-    red_count = 0
-    yellow_count = 0
-    green_count = 0
-
+    color_counts = {'Red': 0, 'Yellow': 0, 'Green': 0}
+    
     with open(file_path, 'r') as file:
         next(file)
         for line in file:
             data = line.strip().split(',')
-            red_count += int(data[0])
-            yellow_count += int(data[1])
-            green_count += int(data[2])
-
-    print("Red =", red_count)
-    print("Yellow =", yellow_count)
-    print("Green =", green_count)
-
-    return {'Red': red_count, 'Yellow': yellow_count, 'Green': green_count}
-
+            color_counts['Red'] += int(data[0])
+            color_counts['Yellow'] += int(data[1])
+            color_counts['Green'] += int(data[2])
+              
+    return color_counts   
+    
 def calculate_color_active_time(file_path):
-    red_time = 0
-    yellow_time = 0
-    green_time = 0
+    color_times = {'Red': 0, 'Yellow': 0, 'Green': 0}
 
     with open(file_path, 'r') as file:
         next(file)
         for line in file:
             data = line.strip().split(',')
             time_active = int(data[3])
-            if data[0] == '1':
-                red_time += time_active
-            elif data[1] == '1':
-                yellow_time += time_active
-            elif data[2] == '1':
-                green_time += time_active
+            color = ['Red', 'Yellow', 'Green'][data.index('1')]
+            color_times[color] += time_active
 
-    print("Red active time =", red_time, "seconds")
-    print("Yellow active time =", yellow_time, "seconds")
-    print("Green active time =", green_time, "seconds")
-
-    return {'Red': red_time, 'Yellow': yellow_time, 'Green': green_time}
+    return color_times
 
 def find_green_active_times(file_path):
     green_active_times = []
@@ -77,28 +61,39 @@ def count_complete_cycles(file_path):
 
 def count_lines_with_errors(file_path):
     error_count = 0
-
+    multi_color_errors = 0
+    no_color_errors = 0
+    
     with open(file_path, 'r') as file:
         next(file)  
         for line in file:
             data = line.strip().split(',')
             active_colors = sum(int(color) for color in data[:3])
 
-            # error
-            if active_colors != 1:
+            if active_colors != 1: # all error
                 error_count += 1
 
-    print("Lines with errors:", error_count)
+            if active_colors > 1:
+                multi_color_errors += 1 
+            
+            if active_colors == 0:
+                no_color_errors += 1
 
-    return error_count
+    print("Lines with errors:", error_count)
+    print("Lines with mutiple colors:", multi_color_errors)
+    print("Lines with no colors:", no_color_errors)
+    
+    return error_count, multi_color_errors, no_color_errors
 
 file_path = 'data.txt' 
 
 print("1. Find the number of red, yellow and green occurrences:")
 color_occurrences = count_color_occurrences(file_path)
+print("Color occurrences:", color_occurrences)
 
 print("\n2. Find how long each colour was active for:")
-color_active_time = calculate_color_active_time(file_path)
+color_times = calculate_color_active_time(file_path)
+print("Color active times:", color_times)
 
 print("\n3. Find all times when Green was active (by time):")
 green_active_times = find_green_active_times(file_path)
@@ -107,4 +102,5 @@ print("\n4. Find the number of complete cycles Red-Yellow-Green-Yellow-Red in th
 cycle_count = count_complete_cycles(file_path)
 
 print("\n5. Find number of lines with mistakes (multiple colours active at the same time or no colours active):")
-error_count = count_lines_with_errors(file_path)
+error_count, multi_color_errors, no_color_errors = count_lines_with_errors(file_path)
+
